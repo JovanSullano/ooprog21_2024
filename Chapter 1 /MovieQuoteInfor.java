5f914a6d4775e7ee0141781e90a8fb372c72a2b3
Jovan Sullano <sullanojovan9@gmail.com>
2024-08-30 08:38:39 +0700

    done

--- Chapter 1 /MovieQuoteInfor.java
+++ Chapter 1 /MovieQuoteInfor.java
@@ -1,9 +1,7 @@
 public class FavoriteQuote {
     public static void main(String[] args) {
-        // Display the quote
         System.out.println("\"There's no place like home.\"");
         
-        // Display the movie details
         System.out.println("Movie: The Wizard of Oz");
         System.out.println("Character: Dorothy");
         System.out.println("Year: 1939");
