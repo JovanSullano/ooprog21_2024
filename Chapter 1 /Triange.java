0f7024ec16e964750cdc22194ba13ec6dc5f660a
Jovan Sullano <sullanojovan9@gmail.com>
2024-08-30 08:39:37 +0700

    done

--- Chapter 1 /Triange.java
+++ Chapter 1 /Triange.java
@@ -1,18 +1,14 @@
 public class TrianglePattern {
     public static void main(String[] args) {
-        int rows = 7; // Number of rows for the triangle
+        int rows = 7; 
 
-        // Generate the triangle pattern
         for (int i = 0; i < rows; i++) {
-            // Print leading spaces
             for (int j = 0; j < rows - i - 1; j++) {
                 System.out.print(" ");
             }
-            // Print the 'T' characters
             for (int k = 0; k < 2 * i + 1; k++) {
                 System.out.print("T");
             }
-            // Move to the next line
             System.out.println();
         }
     }
