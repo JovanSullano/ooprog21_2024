d3de9fe26256c35b005a8be1d0d01b5d59235b42
Jovan Sullano <sullanojovan9@gmail.com>
2024-09-20 09:15:15 +0700

    done

---
+++ Chapter 3/FirstAddressExcercise.java
@@ -0,0 +1,18 @@
+public class FirstAddressExcercise {
+
+   public void displayAddress(){
+   
+         System.out.println("18F, Block C, No.18, Kefa RD");
+         System.out.println("Nanshan, Shenzhen, China");
+         System.out.println("Company: Oppo");
+         
+         }
+         
+   public static void main(String[] args) {
+   
+         FirstAddressExcercise j = new FirstAddressExcercise();
+         j.displayAddress();
+         
+       }   
+   }
+      
